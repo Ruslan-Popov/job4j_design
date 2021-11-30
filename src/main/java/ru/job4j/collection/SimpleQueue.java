361b3279a925cb7cd9931e3b10766c38649fa5af
Ruslan-Popov <ruslan_popov@mail.ru>
2021-12-01 02:31:31 +0300

    2.1.3.5. Очередь на двух стеках [#160]

---
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -0,0 +1,50 @@
+package ru.job4j.collection;
+
+/**
+ * Класс описывает простейшую очередь по принципу FIFO, основанную на двух стеках LIFO
+ * @param <T> тип добавляемых ссылочных переменных
+ * @author Ruslan Popov (ruslan_popov@mail.ru)
+ * @version 1.0
+ */
+public class SimpleQueue<T> {
+    /**
+     * основной стек
+     */
+    private final SimpleStack<T> in = new SimpleStack<>();
+    /**
+     * вспомогательный стек
+     */
+    private final SimpleStack<T> out = new SimpleStack<>();
+    /**
+     * размер очереди
+     */
+    private int size = 0;
+
+    /**
+     * Метод возвращает первое значение и удаляет его
+     * @return возвращает удаленное первое значение
+     */
+    public T poll() {
+        size--;
+        return in.pop();
+    }
+
+    /**
+     * Метод помещает значение в конец очереди.
+     * @param value элемент, который добавляется в конец очереди
+     */
+    public void push(T value) {
+        int tmpSize = size;
+        while (tmpSize > 0) {
+            out.push(in.pop());
+            tmpSize--;
+        }
+        in.push(value);
+        tmpSize = size;
+        while (tmpSize != 0) {
+            in.push(out.pop());
+            tmpSize--;
+        }
+        size++;
+    }
+}
