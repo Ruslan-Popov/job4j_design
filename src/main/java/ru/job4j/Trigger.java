db9d2bee94f6ed044ded9cd03f65e0b1f8539d7f
Ruslan-Popov <ruslan_popov@mail.ru>
2021-12-19 19:48:21 +0300

    2.1.5.1. Создать модель User [#999]

--- src/main/java/ru/job4j/Trigger.java
+++ src/main/java/ru/job4j/Trigger.java
@@ -1,7 +1,24 @@
 package ru.job4j;
 
 public class Trigger {
-    public int someLogic() {
-        return 1;
+    public static String binary(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 31; i++) {
+            sb.append(num % 2 == 0 ? 0 : 1);
+            sb.append((i + 1) % 8 == 0 ? " " : "");
+            num /= 2;
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        int a = -51;
+        System.out.println(binary(a));
+        int b = a >>> 2;
+//        int c = a & b;
+        System.out.println(binary(b));
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+//        System.out.println("с = " + c);
     }
 }
