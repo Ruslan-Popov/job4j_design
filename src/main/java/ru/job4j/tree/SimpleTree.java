fd987b2b0e13cda16e5dc4f5a4b236f5b3fdb5fd
Ruslan-Popov <ruslan_popov@mail.ru>
2022-01-30 02:25:32 +0300

    2.1.6.1. Создать элементарную структуру дерева [#1711]

---
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -0,0 +1,57 @@
+package ru.job4j.tree;
+
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Queue;
+
+public class SimpleTree<E> implements Tree<E> {
+    private final Node<E> root;
+
+    public SimpleTree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean rsl = false;
+        if (this.findBy(parent).isPresent() && this.findBy(child).isEmpty()) {
+            this.findBy(parent).get().children.add(new Node<>(child));
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SimpleTree<?> that = (SimpleTree<?>) o;
+        return Objects.equals(root, that.root);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(root);
+    }
+}
