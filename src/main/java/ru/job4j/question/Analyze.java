a33fdb235466cf5f982067960a485e913b867e0d
Ruslan-Popov <ruslan_popov@mail.ru>
2022-02-09 00:33:21 +0300

    2.1.7.2. Статистика по коллекции. [#45889]

--- src/main/java/ru/job4j/question/Analyze.java
+++ src/main/java/ru/job4j/question/Analyze.java
@@ -4,27 +4,28 @@ import java.util.*;
 
 public class Analyze {
     public static Info diff(Set<User> previous, Set<User> current) {
+
         Info info = new Info(0, 0, 0);
+        Map<Integer, String> prev = new HashMap<>();
+        Map<Integer, String> curr = new HashMap<>();
 
         for (User p : previous) {
-            info.setDeleted(info.getDeleted() + 1);
-            for (User c : current) {
-                if (c.getId() == p.getId()) {
-                    info.setDeleted(info.getDeleted() - 1);
-                }
-
-                if (c.getId() == p.getId() && !c.getName().equals(p.getName())) {
-                    info.setChanged(info.getChanged() + 1);
-                }
+            prev.put(p.getId(), p.getName());
+        }
+        for (User c : current) {
+            curr.put(c.getId(), c.getName());
+        }
+        for (Integer i : curr.keySet()) {
+            if (!prev.containsKey(i)) {
+                info.setAdded(info.getAdded() + 1);
+            }
+            if (prev.containsKey(i) && !prev.containsValue(curr.get(i))) {
+                info.setChanged(info.getChanged() + 1);
             }
         }
-
-        for (User c : current) {
-            info.setAdded(info.getAdded() + 1);
-            for (User p : previous) {
-                if (c.getId() == (p.getId())) {
-                    info.setAdded(info.getAdded() - 1);
-                }
+        for (Integer i : prev.keySet()) {
+            if (!curr.containsKey(i)) {
+                info.setDeleted(info.getDeleted() + 1);
             }
         }
         return info;
