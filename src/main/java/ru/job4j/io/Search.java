932c2cb6f1abd4fb336ddf7c1330bb28cb30110d
Ruslan-Popov <ruslan_popov@mail.ru>
2022-02-19 20:04:02 +0300

    2.2.1.4.0. File [#252491 #265683]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -10,7 +10,6 @@ import java.util.function.Predicate;
 public class Search {
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
-        /*Files.walkFileTree(start, new PrintFiles());*/
         search(start, p -> p.toFile().getName().endsWith(".txt")).forEach(System.out::println);
     }
 
