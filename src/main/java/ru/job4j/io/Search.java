46f64143b636b03b48965635eba2af23e5cd1b4e
Ruslan-Popov <ruslan_popov@mail.ru>
2022-02-21 23:10:39 +0300

    2.2.1.5. Валидация параметров запуска. [#246865 #267045]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,11 +9,18 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length < 2) {
+        if (args.length != 2) {
             throw new IllegalArgumentException("Root folder and extension to search must be specified. Usage java -jar target/search.jar ROOT_FOLDER EXTENSION");
         }
         Path start = Paths.get(args[0]);
-        search(start, p -> p.toFile().getName().endsWith("." + args[1])).forEach(System.out::println);
+        String extension = args[1];
+        if (!Files.isDirectory(start)) {
+            throw new IllegalArgumentException("Root folder must be directory");
+        }
+        if (!extension.startsWith(".")) {
+            throw new IllegalArgumentException("Extension must start with \".\"");
+        }
+        search(start, p -> p.toFile().getName().endsWith(extension)).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
