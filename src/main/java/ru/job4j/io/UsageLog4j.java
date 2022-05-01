d7dffe930ce7b5a3ccaca4e99d4e8d4d5392f388
Ruslan-Popov <ruslan_popov@mail.ru>
2022-05-02 00:14:06 +0300

    2.2.3.3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,10 +7,16 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte wheels = 4;
+        short colors = 3;
+        int age = 6;
+        long friends = 2;
+        float length = 5.0f;
+        double maxSpeed = 60.0;
+        boolean isMainCharacter = true;
+        char favouriteLetter = 'U';
+        LOG.debug("Hero info wheels : {}, colors : {}, age : {}, friends : {}, length : {}, maxSpeed : {}, "
+                + "is main character : {}, favourite letter : {}", wheels, colors, age, friends, length, maxSpeed,
+                 isMainCharacter, favouriteLetter);
     }
 }
